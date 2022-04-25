c81f6f303aab4db8b831fec8d0bd065604420f8f
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2022-04-25 15:48:31 -0400

    implemented Knight legal_move_shape

--- Knight.cpp
+++ Knight.cpp
@@ -3,9 +3,20 @@
 namespace Chess
 {
   bool Knight::legal_move_shape(const Position& start, const Position& end) const {
-    /////////////////////////
-    // [REPLACE THIS STUB] //
-    /////////////////////////
+
+    if (start.first == end.first + 1 || start.first == end.first - 1) {
+      if (start.second == end.second + 2 || start.second == end.second - 2) {
+        return true;
+      }
+    }
+
+    if (start.first == end.first + 2 || start.first == end.first - 2) {
+      if (start.second == end.second + 1 || start.second == end.second - 1) {
+        return true;
+      }
+    }
+
     return false;
+    
   }
 }

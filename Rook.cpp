1a3624e00cfbf545b06b1fc5c69895281602653f
Kenneth Elsman <98336240+Kenneth-E@users.noreply.github.com>
2022-04-26 11:26:15 -0400

    Rook.cpp added minor comments

--- Rook.cpp
+++ Rook.cpp
@@ -4,10 +4,12 @@ namespace Chess
 {
   bool Rook::legal_move_shape(const Position& start, const Position& end) const {
 
+    // vertical movement
     if (start.first == end.first && start.second != end.second) {
       return true;
     }
 
+    // horizontal movement
     if (start.first != end.first && start.second == end.second) {
       return true;
     }
